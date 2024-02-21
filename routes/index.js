07dd1f78e836975cac1ab1c220ef13ae74ac7f8f
Cjmin-korean <jmin9003@naver.com>
2024-02-21 09:42:54 +0900

    24010221

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 // routes/index.jsvert
 const multer = require('multer');
+const express = require('express');
 const xlsx = require('xlsx');
 const fs = require('fs');
 const sql = require("mssql");
+
 module.exports = function (app) {
     const sql = require('mssql');
+    // const poolConnect = pool.connect();
     var config = {
         user: 'pswel1',
         password: '1234',
@@ -15,6 +18,8 @@ module.exports = function (app) {
             enableArithAbort: true
         }
     };
+    const pool = new sql.ConnectionPool(config);
+
     // **** start 
     sql.connect(config).then(pool => {
         app.post('/api/mainall', function (req, res) {
@@ -150,7 +155,7 @@ module.exports = function (app) {
             return pool.request()
                 // .input('searchText', sql.NVarChar, req.body.searchText)
                 .query(
-                    "select * from pwmain"
+                    "select * from pwmain order by customer,cartype asc"
                 )
                 .then(result => {
                     res.json(result.recordset);
@@ -369,6 +374,167 @@ module.exports = function (app) {
 
     });
     // **** finish
+    // **** start       
+    sql.connect(config).then(pool => {
+        app.post('/api/insertpwmain', function (req, res) {
+            res.header("Access-Control-Allow-Origin", "*");
+
+
+            return pool.request()
+                .input('customer', sql.NVarChar, req.body.customer)
+                .input('cartype', sql.NVarChar, req.body.cartype)
+                .input('productnumber', sql.NVarChar, req.body.productnumber)
+                .input('sub', sql.NVarChar, req.body.sub)
+                .input('productname', sql.NVarChar, req.body.productname)
+                .input('marchine', sql.NVarChar, req.body.marchine)
+                .input('touch', sql.NVarChar, req.body.touch)
+                .input('sanum', sql.NVarChar, req.body.sanum)
+                .input('sapum', sql.NVarChar, req.body.sapum)
+                .input('sacount', sql.NVarChar, req.body.sacount)
+                .input('a', sql.NVarChar, req.body.a)
+                .input('b', sql.NVarChar, req.body.b)
+                .input('c', sql.NVarChar, req.body.c)
+                .input('d', sql.NVarChar, req.body.d)
+                .input('e', sql.NVarChar, req.body.e)
+                .input('f', sql.NVarChar, req.body.f)
+                .input('g', sql.NVarChar, req.body.g)
+                .input('h', sql.NVarChar, req.body.h)
+                .input('i', sql.NVarChar, req.body.i)
+                .input('j', sql.NVarChar, req.body.j)
+                .input('spec', sql.NVarChar, req.body.spec)
+                .input('t', sql.NVarChar, req.body.t)
+
+
+                .query(
+                    " INSERT INTO pwmain (customer, cartype, productnumber, sub, productname, marchine, touch, sanum, sapum, sacount, a, b, c, d, e, f, g, h, i, j, spec, t) " +
+                    " values(@customer, @cartype, @productnumber, @sub, @productname, @marchine, @touch, @sanum, @sapum, @sacount, @a, @b, @c, @d, @e, @f, @g, @h, @i, @j, @spec, @t) "
+                )
+
+                .then(result => {
+                    res.json(result.recordset);
+                    res.end();
+
+
+                });
+        });
+
+    });
+    // **** finish
+    sql.connect(config).then(pool => {
+        app.post('/api/deletepwmain', function (req, res) {
+            res.header("Access-Control-Allow-Origin", "*");
+
+            var ids = req.body.ids; // 클라이언트로부터 받은 ID 배열
+
+            // 각 ID에 대해 삭제 쿼리를 실행합니다.
+            Promise.all(ids.map(id => {
+                return pool.request()
+                    .input('id', sql.Int, id)
+                    .query("DELETE FROM pwmain WHERE id=@id");
+            }))
+                .then(results => {
+                    // 모든 삭제 작업이 완료되면 결과를 클라이언트로 응답합니다.
+                    res.json(results.map(result => result.recordset));
+                })
+                .catch(err => {
+                    // 오류가 발생한 경우 오류 메시지를 클라이언트에게 보냅니다.
+                    res.status(500).send(err.message);
+                });
+        });
+    })
+
+
+    // **** start       
+    sql.connect(config).then(pool => {
+        app.post('/api/updatepwmain', function (req, res) {
+            res.header("Access-Control-Allow-Origin", "*");
+
+
+            return pool.request()
+                .input('customer', sql.NVarChar, req.body.customer)
+                .input('cartype', sql.NVarChar, req.body.cartype)
+                .input('productnumber', sql.NVarChar, req.body.productnumber)
+                .input('sub', sql.NVarChar, req.body.sub)
+                .input('productname', sql.NVarChar, req.body.productname)
+                .input('marchine', sql.NVarChar, req.body.marchine)
+                .input('touch', sql.NVarChar, req.body.touch)
+                .input('sanum', sql.NVarChar, req.body.sanum)
+                .input('sapum', sql.NVarChar, req.body.sapum)
+                .input('sacount', sql.NVarChar, req.body.sacount)
+                .input('a', sql.NVarChar, req.body.a)
+                .input('b', sql.NVarChar, req.body.b)
+                .input('c', sql.NVarChar, req.body.c)
+                .input('d', sql.NVarChar, req.body.d)
+                .input('e', sql.NVarChar, req.body.e)
+                .input('f', sql.NVarChar, req.body.f)
+                .input('g', sql.NVarChar, req.body.g)
+                .input('h', sql.NVarChar, req.body.h)
+                .input('i', sql.NVarChar, req.body.i)
+                .input('j', sql.NVarChar, req.body.j)
+                .input('spec', sql.NVarChar, req.body.spec)
+                .input('t', sql.NVarChar, req.body.t)
+                .input('t', sql.Int, req.body.id)
+
+
+                .query(
+                    "update pwmain set customer=@customer, cartype=@cartype, productnumber=@productnumber " +
+                    " , sub=@sub, productname=@productname, marchine=@marchine, touch=@touch, sanum=@sanum, sapum=@sapum, " +
+                    " sacount=@sacount, a=@a, b=@b, c=@c, d=@d, e=@e, f=@f, g=@g, h=@h, i=@i, j=@j, spec=@spec, t=@t where id=@id"
+                )
+
+                .then(result => {
+                    res.json(result.recordset);
+                    res.end();
+
+
+                });
+        });
+
+    });
+    // **** finish
+
+    const storage = multer.memoryStorage();
+    const upload = multer({ storage: storage });
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+
+
+    sql.connect(config).then(pool => {
+        app.use(express.json());
+
+        // CORS setup
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            next();
+        });
+
+        // Image upload endpoint
+        app.post('/api/insertimg', upload.single('file_give'), (req, res) => {
+            const imgData = req.file.buffer;
+
+            console.log('Received image data:', imgData.length, 'bytes');
+
+            const request = pool.request();
+            request.input('img', sql.VarBinary, imgData);
+
+            request.query("INSERT INTO test (img) VALUES (@img)", (err, result) => {
+                if (err) {
+                    console.error('Error:', err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    console.log('Image inserted successfully');
+                    res.json({ msg: 'Image uploaded successfully' });
+                }
+            });
+        });
+
+
+    }).catch(err => console.log('SQL Server Connection Error:', err));
+
+
     // **** start       
     sql.connect(config).then(pool => {
         app.post('/api/updatemain', function (req, res) {
