4e5e5aa20c450dac67ab2a366d6f5986fea94a0c
Cjmin-korean <jmin9003@naver.com>
2024-04-22 15:32:58 +0900

    24010422

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-var cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
+const app = express();
+const PORT = 3001;
 
-console.log('ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=')
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8080;
-var router = require('./routes')(app);
-var server = app.listen(port, function () { console.log("Express server has started on port " + port) });
-
-const PORT = 3001;
 
 const corsOptions = {
     origin: '*',
     credentials: true,
 };
 
-console.log('corsOptions', corsOptions)
-console.log('process.env', process.env.PORT)
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
@@ -32,23 +27,47 @@ app.use((req, res, next) => {
 // 정적 파일 불러오기
 app.use(express.static(__dirname + "/views"));
 
-app.get("/", cors(), (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/html/index.html");
 });
-app.get("/main", cors(), (req, res) => {
+
+app.get("/main", (req, res) => {
     res.sendFile(__dirname + "/views/html/index.html");
 });
-app.get("/mainall", cors(), (req, res) => {
+
+app.get("/mainall", (req, res) => {
     res.sendFile(__dirname + "/views/html/mainmenu.html");
 });
-app.get("/userui", cors(), (req, res) => {
+
+app.get("/userui", (req, res) => {
     res.sendFile(__dirname + "/views/html/linecode.html");
 });
-app.get("/upload", cors(), (req, res) => {
-    res.sendFile(__dirname + "/views/html/0.html");
+
+app.get("/upload", (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
 });
 
+// 파일 업로드를 위한 multer 설정
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
 
+app.post('/uploads', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    res.send('ok');
+});
+
+// uploads 폴더의 정적 파일 불러오기
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(PORT, () => {
     console.log(`Listen : ${PORT}`);
