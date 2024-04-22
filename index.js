b92aa6ec791c4db5ad1020eb15b2e07389399da0
Cjmin-korean <jmin9003@naver.com>
2024-04-22 15:04:32 +0900

    24010422

--- index.js
+++ index.js
@@ -1,67 +1,55 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const path = require('path');
+var cors = require('cors');
 
-dotenv.config();
-const app = express();
-app.set('port', process.env.PORT || 3000);
 
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-  name: 'session-cookie',
-}));
+console.log('ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8080;
+var router = require('./routes')(app);
+var server = app.listen(port, function () { console.log("Express server has started on port " + port) });
 
-const multer = require('multer');
-const fs = require('fs');
+const PORT = 3001;
 
-const uploadDir = path.join(__dirname, 'uploads');
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+};
 
-try {
-  fs.readdirSync(uploadDir);
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync(uploadDir);
-}
+console.log('corsOptions', corsOptions)
+console.log('process.env', process.env.PORT)
+app.use(cors(corsOptions));
 
-const storage = multer.diskStorage({
-  destination(req, file, done) {
-    done(null, uploadDir);
-  },
-  filename(req, file, done) {
-    const ext = path.extname(file.originalname);
-    done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-  },
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://gmauto.xyz");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
+// 정적 파일 불러오기
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", cors(), (req, res) => {
+    res.sendFile(__dirname + "/views/html/index.html");
+});
+app.get("/main", cors(), (req, res) => {
+    res.sendFile(__dirname + "/views/html/index.html");
+});
+app.get("/mainall", cors(), (req, res) => {
+    res.sendFile(__dirname + "/views/html/mainmenu.html");
+});
+app.get("/userui", cors(), (req, res) => {
+    res.sendFile(__dirname + "/views/html/linecode.html");
+});
+app.get("/upload", cors(), (req, res) => {
+    res.sendFile(__dirname + "/views/html/0.html");
 });
 
-// 업로드 라우트 추가
-app.get('/upload', (req, res) => {
-  res.sendFile(path.join(__dirname, 'multipart.html'));
-});
 
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log(req.file);
-  res.send('ok');
-});
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 실행됩니다');
+app.listen(PORT, () => {
+    console.log(`Listen : ${PORT}`);
 });
