907d7c46497a6f13ce8afa16830fdf8f7147d031
Cjmin-korean <jmin9003@naver.com>
2024-04-23 08:29:58 +0900

    24010422

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var multer = require('multer');
 var path = require('path');
 var fs = require('fs');
 
+
+console.log('ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=ReStart=>=>=>=>=>=>=>=')
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -10,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 var router = require('./routes')(app);
+var server = app.listen(port, function () { console.log("Express server has started on port " + port) });
 
 const PORT = 3001;
 
@@ -48,17 +51,11 @@ app.get("/upload", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/html/pwmain.html'));
 });
 
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-}
 // 파일 업로드를 위한 multer 설정
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, done) {
-            done(null, path.join(__dirname, 'uploads')); // uploads 폴더에 저장
+            done(null, path.join(__dirname, 'views', 'img')); // 파일을 html 폴더의 상위 폴더에 저장
         },
         filename(req, file, done) {
             const ext = path.extname(file.originalname);
@@ -79,12 +76,8 @@ app.post('/upload', upload.single('image'), (req, res) => {
 });
 
 // uploads 폴더의 정적 파일 불러오기
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/upload', express.static(path.join(__dirname, 'views', 'img')));
 
-var server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listen : ${PORT}`);
-});
-
-server.on('listening', () => {
-    console.log('Server is ReStarted');
 });
